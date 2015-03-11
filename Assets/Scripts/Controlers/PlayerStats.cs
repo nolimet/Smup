3e848937cb82bf57e0ab6380f83e3e68bd52b00a
Jesse Stam <nolimet@stamdata.nl>
2015-03-11 17:33:38 +0100

    Added a Energy bar

---
+++ Assets/Scripts/Controlers/PlayerStats.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+    [SerializeField]
+    Bar Energy;
+    public float currentEnergy = 0;
+    public float maxEnergy = 200;
+
+    void Start()
+    {
+        Energy.init(maxEnergy, 2f);
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        currentEnergy += 16f * Time.deltaTime;
+        Energy.UpdateSize(currentEnergy);
+	}
+}
---
+++ Assets/Scripts/ValueClasses/Bar.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[System.Serializable]
+public class Bar
+{
+    public RectTransform Normal, Overload;
+    public Text valueText;
+    Vector2 StartSize;
+    float maxNormal, overloadFactor, frac;
+
+    /// <summary>
+    /// Start function for A bar
+    /// </summary>
+    /// <param name="maxNormal">Used to draw the overload at the right moment</param>
+    /// <param name="maxOverloadFactor">How many times more can the overload hold than the Normal</param>
+    public void init(float maxNormal, float overloadFactor)
+    {
+        this.overloadFactor = overloadFactor;
+        this.maxNormal = maxNormal;
+
+        StartSize = Normal.sizeDelta;
+        frac = 1f / maxNormal;
+    }
+
+    /// <summary>
+    /// Input the noneClamped value
+    /// </summary>
+    /// <param name="value">the level it should display</param>
+    public void UpdateSize(float value)
+    {
+        if (valueText)
+            valueText.text = Mathf.Floor(value).ToString() + " / " + Mathf.Floor(maxNormal).ToString();
+
+
+        float calc = (frac * value);
+        if (value < maxNormal)
+        {
+            Normal.sizeDelta = new Vector2(StartSize.x * calc, StartSize.y);
+            Overload.sizeDelta = new Vector2(0.01f, StartSize.y);
+        }
+        else if (value <= maxNormal * overloadFactor)
+        {
+            Normal.sizeDelta = StartSize;
+            Overload.sizeDelta = new Vector2(StartSize.x * ((calc - 1f) / overloadFactor), StartSize.y);
+        }
+
+    }
+
+
+}
