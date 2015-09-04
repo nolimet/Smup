c9cc305326426add8ad15e798b52b0111589a0c5
Nolimet <nolimet@stamdata.nl>
2015-09-04 15:23:52 +0200

    added Catagories

---
+++ Assets/Scripts/Menus/MenuParts/UpgradeCatagory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UpgradeCatagory : MonoBehaviour
+{
+    public Text Name;
+    public Text Discription;
+    public Button ButtonObj;
+
+    public void SetText(string Name, string Discription)
+    {
+        this.Name.text = Name;
+        this.Discription.text = Discription;
+    }
+}
--- Assets/Scripts/UpgradeSystem/UpgradeMenu.cs
+++ Assets/Scripts/UpgradeSystem/UpgradeMenu.cs
@@ -13,9 +13,12 @@ public class UpgradeMenu : MonoBehaviour
 
     public Text UpgradeCurrency;
     public UpgradeItem ParentUpgrade;
+    public UpgradeCatagory parentCatagory;
     public Transform ContentParent;
 
     public List<UpgradeObject> Upgrades;
+    public List<CatagoryObject> cats;
+    CatagoryObject currentCatagory;
 
     private static UpgradeMenu instance;
     public static UpgradeData upgradeData { get{ return instance.upgrades; } }
@@ -94,7 +97,21 @@ public class UpgradeMenu : MonoBehaviour
         e.Name.text = name;
         e.Discription.text = discription;
 
-        Upgrades.Add(new UpgradeObject(refrence, StartCost,MaxLevel, Mult, e));
+        UpgradeObject u = new UpgradeObject(refrence, StartCost, MaxLevel, Mult, e);
+        currentCatagory.addNew(u);
+        Upgrades.Add(u);
+    }
+
+    void addCatagory(string name, string discription)
+    {
+       UpgradeCatagory u = Instantiate(parentCatagory);
+        u.transform.SetParent(ContentParent,false);
+
+        CatagoryObject o = new CatagoryObject(u, name, discription);
+
+        currentCatagory = o;
+        o.ToggleAll();
+        cats.Add(o);
     }
 
     /// <summary>
@@ -104,19 +121,28 @@ public class UpgradeMenu : MonoBehaviour
     {
 
         //scrapCollection
+        addCatagory("Scrap Collection", "Enchance your scrap collection ability");
         addUpgrade("ScrapConversionRate", 30, 1.3f, "Scrap sell rate", "Improves for how much you sell each piece of scrap");
         addUpgrade("ScrapCollectionRange", 145, 1.3f, "Scrap Collection Range", "Makes the collection range bigger");
         addUpgrade("ScrapCollectionSpeed", 145, 1.5f, "Scrap Collection Speed", "Improves how fast the scrap moves towards you");
 
         //Hull
+        addCatagory("Ship Upgrades", "Upgrades that make your ship strong, faster and generaly better");
         addUpgrade("hullUpgradeLevel", 50, 1.4f, "Hull Upgrade", "Upgrading the hull allows the ship to take more hits");
+        addUpgrade("armorUpgradeLevel", 100, 1.4f, "Armor Upgrade", "Upgrades the ammount of armor the ship has");
+        //Shield
+        addUpgrade("UnlockedShield", 1200, 0, "Unlock Shield", "Unlocks the shield");
+        addUpgrade("shieldGeneratorLevel", 1300, 1.2f, "Shield Regeneration Speed", "Increases the regeneration speed of the shield");
+        addUpgrade("shieldCapacitorLevel", 1800, 1.2f, "Shield capacity upgrade", "Increases the capacity of the shield");
 
         //shotgun
+        addCatagory("ShotGun Upgrades", " Upgrades for your shotgun");
         addUpgrade("UnlockedShotgun", 300, 0, "Fragments per shotgun shot", "Increases the number of framents shot out per shot");
         addUpgrade("ShotGunBulletsPerShot", 75,1.3f, "Fragments per shotgun shot", "Increases the number of framents shot out per shot",6);
         addUpgrade("ShotGunDamagePerFragment",125,1.3f, "Damage per frament", "Increases the damage each frament does");
 
         //machineGun
+        addCatagory("Machinegun Upgrades", "Upgrades for your machinegun");
         addUpgrade("UnlockedMachineGun",700,0, "Unlock Machinegun", "Unlocks the Machinegun");
         addUpgrade("MachineGunBulletsPerSecond", 250, 1.2f, "Firerate MachineGun", "Increases number of bullets shot out by the machine gun every second",20);
         addUpgrade("MachineGunDamagePerBullet", 400, 1.1f, "Damage per bullet MachineGun", "Increases the damage each bullet of the machine gun does");
@@ -137,6 +163,9 @@ public class UpgradeMenu : MonoBehaviour
 
         int Level = 0, MaxLevel =-1;
         string refrence;
+
+        public bool enabled { set { visual.gameObject.SetActive(value); } }
+
         /// <summary>
         /// 
         /// </summary>
@@ -238,8 +267,6 @@ public class UpgradeMenu : MonoBehaviour
             }
         }
 
-        
-
         bool canBuy(bool substract)
         {
             if (upgradeData.UpgradeCurrency >= GetPrice() && (MaxLevel<0 || MaxLevel>0 && Level < MaxLevel) && (Mult>0 || Mult<=0 && Level<=0))
@@ -283,14 +310,38 @@ public class UpgradeMenu : MonoBehaviour
             return false;
         }
     }
-}
 
-public class UpgradeCatagory : MonoBehaviour
-{
-    Text Name;
-
-    public void SetText(string Name)
+    [System.Serializable]
+    public class CatagoryObject
     {
-        this.Name.text = Name;
+        UpgradeCatagory o;
+        List<UpgradeObject> subObjs = new List<UpgradeObject>();
+        bool active = true;
+
+        public CatagoryObject (UpgradeCatagory o, string name, string discription)
+        {
+            this.o = o;
+            o.SetText(name, discription);
+
+            o.ButtonObj.onClick.AddListener(delegate
+            {
+                ToggleAll();
+            });
+        }
+
+        public void addNew(UpgradeObject o)
+        {
+            o.enabled = active;
+            subObjs.Add(o);
+        }
+
+        public void ToggleAll()
+        {
+            active = !active;
+            foreach (UpgradeObject o in subObjs)
+                o.enabled = active;
+        }
     }
 }
+
+
