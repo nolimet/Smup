9d6587aaf8a8884cbde7055d235af597c6ab102c
Jesse  Stam <jesse.stam@beyondsports.nl>
2025-09-10 15:30:36 +0200

    Improved the debug spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,12 +8,28 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private string enemyId;
     [SerializeField] [ReadOnly] private Enemy currentTarget;
 
-    private void Start()
+    [SerializeField] private float spawnDelay = 0.2f;
+    private float _spawnTimer;
+
+    private void Spawn()
     {
         currentTarget = EnemyPool.Instance.GetObject(enemyId);
         currentTarget.moveSpeed.SetOverride(0);
         currentTarget.transform.position = transform.position;
-        var rigidbody2D = currentTarget.GetComponent<Rigidbody2D>();
-        rigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
+        var rig2d = currentTarget.GetComponent<Rigidbody2D>();
+        rig2d.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+
+    private void Update()
+    {
+        if (!currentTarget || currentTarget.Health <= 0)
+        {
+            _spawnTimer -= Time.deltaTime;
+            if (_spawnTimer <= 0)
+            {
+                Spawn();
+                _spawnTimer = spawnDelay;
+            }
+        }
     }
 }
