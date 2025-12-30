a9244435c71ff420c6e7e68e0ab94169007416f2
jesse stam <nolimet@stamdata.nl>
2025-12-30 16:02:03 +0100

    Updated namespaces in `PickupManager` and `GameManager` scripts to align with `Entities` and `Pools`. Simplified formatting and conditional logic across both scripts.
    Signed-off-by: jesse stam <nolimet@stamdata.nl>

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,4 @@
-﻿using Player;
+﻿using Entities.Player;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,65 +7,57 @@ using Util.DebugHelpers;
 
 namespace Managers
 {
-	public class GameManager : MonoBehaviour
-	{
-		[SerializeField] private PlayerStats playerStats;
-		[SerializeField] private PlayerWeaponControler playerWeaponControler;
-		[SerializeField] private PickupManager pickupManager;
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private PlayerStats playerStats;
+        [SerializeField] private PlayerWeaponControler playerWeaponControler;
+        [SerializeField] private PickupManager pickupManager;
 
-		// ReSharper disable once InconsistentNaming
-		[ShowInInspector] private UpgradeData _upgrades => Application.isPlaying ? SaveDataManager.Upgrades : null;
+        // ReSharper disable once InconsistentNaming
+        [ShowInInspector] private UpgradeData _upgrades => Application.isPlaying ? SaveDataManager.Upgrades : null;
 
-		public static PlayerStats Stats => Instance.playerStats;
-		public static PlayerWeaponControler WeaponController => Instance.playerWeaponControler;
-		public static PickupManager PickupManager => Instance.pickupManager;
+        public static PlayerStats Stats => Instance.playerStats;
+        public static PlayerWeaponControler WeaponController => Instance.playerWeaponControler;
+        public static PickupManager PickupManager => Instance.pickupManager;
 
-		public static GameManager Instance
-		{
-			get
-			{
-				if (_instance == null || !_instance)
-				{
-					FindAnyObjectByType<GameManager>();
-				}
+        public static GameManager Instance
+        {
+            get
+            {
+                if (_instance == null || !_instance) FindAnyObjectByType<GameManager>();
 
-				if (_instance == null || !_instance)
-				{
-					Debug.LogWarning("No GameManager object found");
-				}
+                if (_instance == null || !_instance) Debug.LogWarning("No GameManager object found");
 
-				return _instance;
-			}
-		}
+                return _instance;
+            }
+        }
 
-		private static GameManager _instance;
+        private static GameManager _instance;
 
-		public void Awake()
-		{
-			if (_instance != null)
-			{
-				Debug.LogError("More than one GameManager!. Destroying duplicate GameManager");
-				Destroy(this);
-				return;
-			}
+        public void Awake()
+        {
+            if (_instance != null)
+            {
+                Debug.LogError("More than one GameManager!. Destroying duplicate GameManager");
+                Destroy(this);
+                return;
+            }
 
-			_instance = this;
+            _instance = this;
 
-			Debugger.DebugEnabled = true;
-		}
+            Debugger.DebugEnabled = true;
+        }
 
-		public void OnDestroy()
-		{
-			_instance = null;
-			SaveDataManager.SaveAll();
-		}
+        public void OnDestroy()
+        {
+            _instance = null;
+            SaveDataManager.SaveAll();
+        }
 
-		private void Update()
-		{
-			if (Input.GetKeyDown(KeyCode.Escape)) //TODO refactor to use a asset refrence. To make sure we returned to the menu
-			{
-				SceneManager.LoadScene(0, LoadSceneMode.Single);
-			}
-		}
-	}
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape)) //TODO refactor to use a asset refrence. To make sure we returned to the menu
+                SceneManager.LoadScene(0, LoadSceneMode.Single);
+        }
+    }
 }
--- Assets/Scripts/Managers/PickupManager.cs
+++ Assets/Scripts/Managers/PickupManager.cs
@@ -1,77 +1,65 @@
-﻿using ObjectPools;
-using Pickups;
+﻿using Entities.Generic;
+using Pools;
 using UnityEngine;
 using Util;
 
 namespace Managers
 {
-	[RequireComponent(typeof(CircleCollider2D))]
-	public class PickupManager : MonoBehaviour
-	{
-		private int _pickupLayer;
-		public float PickedUpScrap { get; private set; }
+    [RequireComponent(typeof(CircleCollider2D))]
+    public class PickupManager : MonoBehaviour
+    {
+        private int _pickupLayer;
+        public float PickedUpScrap { get; private set; }
 
-		// Use this for initialization
-		private void Start()
-		{
-			_pickupLayer = LayerMask.NameToLayer("Pickup");
-			GetComponent<CircleCollider2D>().radius = 7 * Mathf.Pow(1.2f, SaveDataManager.Upgrades.ScrapCollectionRange);
-		}
+        // Use this for initialization
+        private void Start()
+        {
+            _pickupLayer = LayerMask.NameToLayer("Pickup");
+            GetComponent<CircleCollider2D>().radius = 7 * Mathf.Pow(1.2f, SaveDataManager.Upgrades.ScrapCollectionRange);
+        }
 
-		private void OnDestroy()
-		{
-			var dat = SaveDataManager.Upgrades;
-			SaveDataManager.Upgrades.upgradeCurrency += Mathf.FloorToInt(PickedUpScrap * ((dat.ScrapConversionRate + 1) * 1.1f));
-		}
+        private void OnDestroy()
+        {
+            var dat = SaveDataManager.Upgrades;
+            SaveDataManager.Upgrades.upgradeCurrency += Mathf.FloorToInt(PickedUpScrap * ((dat.ScrapConversionRate + 1) * 1.1f));
+        }
 
-		public void OnTriggerStay2D(Collider2D col)
-		{
-			if (col.gameObject.layer == _pickupLayer)
-			{
-				var rigidBody = col.GetComponent<Rigidbody2D>();
-				if (!rigidBody)
-				{
-					return;
-				}
+        public void OnTriggerStay2D(Collider2D col)
+        {
+            if (col.gameObject.layer == _pickupLayer)
+            {
+                var rigidBody = col.GetComponent<Rigidbody2D>();
+                if (!rigidBody) return;
 
-				var speed = rigidBody.linearVelocity.GetLength();
-				if (speed < 0)
-				{
-					speed *= -1;
-				}
+                var speed = rigidBody.linearVelocity.GetLength();
+                if (speed < 0) speed *= -1;
 
-				if (speed < 20)
-				{
-					Vector2 v2 = transform.position - col.transform.position;
-					v2.Normalize();
-					rigidBody.AddForce(v2 * (20f * Mathf.Pow(1.5f, SaveDataManager.Upgrades.ScrapCollectionSpeed)) * Time.deltaTime);
-				}
-			}
-		}
+                if (speed < 20)
+                {
+                    Vector2 v2 = transform.position - col.transform.position;
+                    v2.Normalize();
+                    rigidBody.AddForce(v2 * (20f * Mathf.Pow(1.5f, SaveDataManager.Upgrades.ScrapCollectionSpeed)) * Time.deltaTime);
+                }
+            }
+        }
 
-		public void OnTriggerExit2D(Collider2D col)
-		{
-			if (col.gameObject.layer == _pickupLayer)
-			{
-				col.GetComponent<Rigidbody2D>().linearDamping = 5f;
-			}
-		}
+        public void OnTriggerExit2D(Collider2D col)
+        {
+            if (col.gameObject.layer == _pickupLayer) col.GetComponent<Rigidbody2D>().linearDamping = 5f;
+        }
 
-		public void OnTriggerEnter2D(Collider2D col)
-		{
-			if (col.gameObject.layer == _pickupLayer)
-			{
-				col.GetComponent<Rigidbody2D>().linearDamping = 1f;
-			}
-		}
+        public void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.gameObject.layer == _pickupLayer) col.GetComponent<Rigidbody2D>().linearDamping = 1f;
+        }
 
-		public void OnCollisionEnter2D(Collision2D col)
-		{
-			if (col.gameObject.layer == _pickupLayer)
-			{
-				ScrapPickupPool.RemovePickup(col.gameObject.GetComponent<ScrapPickup>());
-				PickedUpScrap += col.gameObject.GetComponent<ScrapPickup>().ScrapValue;
-			}
-		}
-	}
+        public void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.layer == _pickupLayer)
+            {
+                ScrapPickupPool.RemovePickup(col.gameObject.GetComponent<ScrapPickup>());
+                PickedUpScrap += col.gameObject.GetComponent<ScrapPickup>().ScrapValue;
+            }
+        }
+    }
 }
