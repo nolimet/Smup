73c4a53d6d1229fe351e012755150a0e20c32b8f
jesse stam <nolimet@stamdata.nl>
2026-01-21 22:29:38 +0100

    Refactored `BulletBoundarySystem` to initialize bounds only once, improving efficiency and clarity.
    Signed-off-by: jesse stam <nolimet@stamdata.nl>

--- Assets/Scripts/Entities/ECS/Bullet/BulletBoundarySystem.cs
+++ Assets/Scripts/Entities/ECS/Bullet/BulletBoundarySystem.cs
@@ -8,31 +8,32 @@ namespace Entities.ECS.Bullet
     [UpdateInGroup(typeof(SimulationSystemGroup))]
     public partial struct BulletBoundarySystem : ISystem
     {
+        private float4 _bounds;
+        private bool _initialized;
+
         public void OnCreate(ref SystemState state)
         {
             state.RequireForUpdate<EndSimulationEntityCommandBufferSystem.Singleton>();
         }
 
-        private float4 _bounds; // x: minX, y: maxX, z: minY, w: maxY
-
         public void OnUpdate(ref SystemState state)
         {
-            // We calculate bounds once per frame. 
-            // In a real shmup, you'd likely use a fixed game area or the camera frustum.
-            var cam = Camera.main;
-            if (cam == null) return;
+            if (!_initialized)
+            {
+                var cam = Camera.main;
+                if (cam == null) return;
 
-            // Get screen bounds in world space (with a small buffer so they don't pop out)
-            var buffer = 1.0f;
-            var bottomLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
-            var topRight = cam.ViewportToWorldPoint(new Vector3(1, 1, cam.nearClipPlane));
+                var bottomLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
+                var topRight = cam.ViewportToWorldPoint(new Vector3(1, 1, cam.nearClipPlane));
 
-            _bounds = new float4(bottomLeft.x - buffer, topRight.x + buffer, bottomLeft.y - buffer, topRight.y + buffer);
+                const float buffer = 1.0f;
+                _bounds = new float4(bottomLeft.x - buffer, topRight.x + buffer, bottomLeft.y - buffer, topRight.y + buffer);
+                _initialized = true;
+            }
 
             var ecbSingleton = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
             var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
 
-            // Schedule the job to check positions
             foreach (var (transform, entity) in SystemAPI.Query<RefRO<LocalTransform>>().WithAll<BulletData>().WithEntityAccess())
             {
                 var pos = transform.ValueRO.Position;
