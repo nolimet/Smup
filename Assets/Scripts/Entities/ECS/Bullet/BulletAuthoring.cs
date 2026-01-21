95fc72674fc1708247ccda2b4ac97b868e44bd00
jesse stam <nolimet@stamdata.nl>
2026-01-21 22:29:50 +0100

    Removed unused `TargetLayerMask` from `BulletData` and cleaned up comments in `BulletAuthoring`.
    Signed-off-by: jesse stam <nolimet@stamdata.nl>

--- Assets/Scripts/Entities/ECS/Bullet/BulletAuthoring.cs
+++ Assets/Scripts/Entities/ECS/Bullet/BulletAuthoring.cs
@@ -8,7 +8,6 @@ namespace Entities.ECS.Bullet
     public struct BulletData : IComponentData
     {
         public float Damage;
-        public uint TargetLayerMask;
     }
 
     public class BulletAuthoring : MonoBehaviour
@@ -19,13 +18,7 @@ namespace Entities.ECS.Bullet
             {
                 var entity = GetEntity(TransformUsageFlags.Dynamic);
 
-                // This makes the Prefab compatible with your Spawner's SetComponentData calls
                 AddComponent(entity, new BulletData());
-
-                // IMPORTANT: If you want to use Unity Physics, 
-                // you must also have a PhysicsBody component on the GameObject
-                // and this component will be baked automatically. 
-                // But we add the data component here for logic.
                 AddComponent(entity, new PhysicsVelocity());
                 AddComponent(entity, PhysicsMass.CreateDynamic(MassProperties.CreateBox(new float3(0.119999997f, 0.0700000003f, 0.200000003f)), 5f));
             }
